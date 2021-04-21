43506e5383198b11b788ca71cd7ea9dd5f3af27e
Kyle Lomen <kylelomen@gmail.com>
2021-04-20 23:23:29 -0700

    First Test

--- src/ILI9486.cpp
+++ src/ILI9486.cpp
@@ -2,3 +2,185 @@
 
 #include <Adafruit_GFX.h>
 #include <Arduino.h>
+
+#define dataSetupTime     20
+#define dataHoldTime      20
+#define readDelay         500
+#define outputDisableTime 100
+#define writeDelay        50
+#define chipSelectDelay   400
+
+extern void        delayNanoseconds(uint32_t);
+static inline void _delayNanoseconds(uint32_t nsec) __attribute__((weakref("delayNanoseconds")));
+static inline void _delayNanoseconds(uint32_t nsec) {
+  delayMicroseconds((nsec + 999) / 1000);
+}
+
+ILI9486::ILI9486(uint8_t d0, uint8_t rd, uint8_t wr, uint8_t dc, int8_t cs = -1, int8_t rst = -1)
+    : ILI9486(d0, d0 + 1, d0 + 2, d0 + 3, d0 + 4, d0 + 5, d0 + 6, d0 + 7, rd, wr, dc, cs, rst) {}
+ILI9486::ILI9486(uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, uint8_t rd,
+                 uint8_t wr, uint8_t dc, int8_t cs = -1, int8_t rst = -1)
+    : Adafruit_GFX(ILI9486_TFTWIDTH, ILI9486_TFTHEIGHT) {
+  _dataPins[0] = d0;
+  _dataPins[1] = d1;
+  _dataPins[2] = d2;
+  _dataPins[3] = d3;
+  _dataPins[4] = d4;
+  _dataPins[5] = d5;
+  _dataPins[6] = d6;
+  _dataPins[7] = d7;
+  for(uint8_t p : _dataPins) {
+    pinMode(p, INPUT);
+  }
+
+  _readPin = rd;
+  pinMode(_readPin, OUTPUT);
+  digitalWrite(_readPin, HIGH);
+
+  _writePin = wr;
+  pinMode(_writePin, OUTPUT);
+  digitalWrite(_writePin, HIGH);
+
+  _dataCommandPin = dc;
+  pinMode(_dataCommandPin, OUTPUT);
+  digitalWrite(_dataCommandPin, HIGH);
+
+  if(cs != -1) {
+    _useChipSelect = true;
+    _chipSelectPin = cs;
+    pinMode(_chipSelectPin, OUTPUT);
+    digitalWrite(_chipSelectPin, HIGH);
+  }
+  else {
+    _useChipSelect = false;
+  }
+  if(rst != -1) {
+    _useReset = true;
+    _resetPin = rst;
+    pinMode(_resetPin, OUTPUT);
+    digitalWrite(_resetPin, LOW);
+  }
+  else {
+    _useReset = false;
+  }
+}
+
+bool ILI9486::begin(bool reset = true) {
+  if(reset) {
+    if(_useReset) {
+      digitalWrite(_resetPin, HIGH);
+    }
+    else {
+      sendCommand(ILI9486_SWRESET);
+    }
+    delay(150);
+  }
+  sendFrame(ILI9486_PWCTR1, 2, (uint8_t[]){0x0d, 0x0d});              //Power Control 1 [0E 0E]
+  sendFrame(ILI9486_PWCTR2, 2, (uint8_t[]){0x43, 0x00});              //Power Control 2 [43 00]
+  sendFrame(ILI9486_PWCTR3, 1, (uint8_t[]){0x00});                    //Power Control 3 [33]
+  sendFrame(ILI9486_VMCTR1, 4, (uint8_t[]){0x00, 0x48, 0x00, 0x48});  //VCOM  Control 1 [00 40 00 40]
+  sendFrame(ILI9486_INVCTR, 1, (uint8_t[]){0x00});                    //Inversion Control [00]
+  sendFrame(ILI9486_DFUNCTR, 3, (uint8_t[]){0x02, 0x02, 0x3B});       // Display Function Control [02 02 3B]
+  sendFrame(ILI9486_GMCTRP1, 15,
+            (uint8_t[]){0x0F, 0x21, 0x1C, 0x0B, 0x0E, 0x08, 0x49, 0x98, 0x38, 0x09, 0x11, 0x03, 0x14, 0x10, 0x00});
+  sendFrame(ILI9486_GMCTRN1, 15,
+            (uint8_t[]){0x0F, 0x2F, 0x2B, 0x0C, 0x0E, 0x06, 0x47, 0x76, 0x37, 0x07, 0x11, 0x04, 0x23, 0x1E, 0x00});
+}
+
+void ILI9486::setBounds(uint16_t xStart, uint16_t yStart, uint16_t xEnd, uint16_t yEnd) {
+  uint8_t buffer[4];
+  buffer[0] = xStart >> 8;
+  buffer[1] = xStart & 0xFF;
+  buffer[0] = xEnd >> 8;
+  buffer[1] = xEnd & 0xFF;
+  sendFrame(ILI9486_PASET, 4, buffer);
+  buffer[0] = yStart >> 8;
+  buffer[1] = yStart & 0xFF;
+  buffer[0] = yEnd >> 8;
+  buffer[1] = yEnd & 0xFF;
+  sendFrame(ILI9486_CASET, 4, buffer);
+}
+
+void ILI9486::drawPixel(int16_t x, int16_t y, uint16_t color) {
+  setBounds(x, y, x, y);
+  sendFrame(ILI9486_RAMWR, 2, (uint8_t*)&color);
+}
+
+void ILI9486::sendCommand(uint8_t command) {
+  for(uint8_t p : _dataPins) {
+    pinMode(p, OUTPUT);
+  }
+  digitalWrite(_dataCommandPin, LOW);
+  delayNanoseconds(dataHoldTime);
+  digitalWrite(_writePin, LOW);
+  for(int i = 0; i < 8; i++) {
+    if(command & (1 << i))
+      digitalWrite(_dataPins[i], HIGH);
+    else
+      digitalWrite(_dataPins[i], LOW);
+  }
+  delayNanoseconds(dataSetupTime);
+  digitalWrite(_writePin, HIGH);
+  delayNanoseconds(writeDelay);
+  for(uint8_t p : _dataPins) {
+    pinMode(p, INPUT);
+  }
+}
+
+void ILI9486::sendData(uint8_t data) {
+  for(uint8_t p : _dataPins) {
+    pinMode(p, OUTPUT);
+  }
+  digitalWrite(_dataCommandPin, HIGH);
+  delayNanoseconds(dataHoldTime);
+  digitalWrite(_writePin, LOW);
+  for(int i = 0; i < 8; i++) {
+    if(data & (1 << i))
+      digitalWrite(_dataPins[i], HIGH);
+    else
+      digitalWrite(_dataPins[i], LOW);
+  }
+  delayNanoseconds(dataSetupTime);
+  digitalWrite(_writePin, HIGH);
+  delayNanoseconds(writeDelay);
+  for(uint8_t p : _dataPins) {
+    pinMode(p, INPUT);
+  }
+}
+
+uint8_t ILI9486::readData() {
+  uint8_t data = 0;
+  digitalWrite(_dataCommandPin, HIGH);
+  delayNanoseconds(dataHoldTime);
+  digitalWrite(_readPin, LOW);
+  delayNanoseconds(readDelay);
+  digitalWrite(_readPin, HIGH);
+  for(int i = 0; i < 8; i++) {
+    if(digitalRead(_dataPins[i]))
+      data |= 1 << i;
+  }
+  delayNanoseconds(outputDisableTime);
+}
+
+void ILI9486::startTransaction() {
+  if(_useChipSelect) {
+    digitalWrite(_chipSelectPin, LOW);
+    delayNanoseconds(chipSelectDelay);
+  }
+}
+
+void ILI9486::endTransaction() {
+  if(_useChipSelect) {
+    digitalWrite(_chipSelectPin, HIGH);
+    delayNanoseconds(chipSelectDelay);
+  }
+}
+
+void ILI9486::sendFrame(uint8_t command, uint8_t length, uint8_t data[]) {
+  startTransaction();
+  sendCommand(command);
+  for(int i = 0; i < length; i++) {
+    sendData(data[i]);
+  }
+  endTransaction();
+}
--- src/ILI9486.h
+++ src/ILI9486.h
@@ -13,29 +13,110 @@
 
 #include <Adafruit_GFX.h>
 
+#define ILI9486_TFTWIDTH  320  ///< ILI9486 max TFT width
+#define ILI9486_TFTHEIGHT 480  ///< ILI9486 max TFT height
+
+#define ILI9486_NOP     0x00  ///< No-op register
+#define ILI9486_SWRESET 0x01  ///< Software reset register
+#define ILI9486_RDDID   0x04  ///< Read display identification information
+#define ILI9486_RDDST   0x09  ///< Read Display Status
+
+#define ILI9486_SLPIN  0x10  ///< Enter Sleep Mode
+#define ILI9486_SLPOUT 0x11  ///< Sleep Out
+#define ILI9486_PTLON  0x12  ///< Partial Mode ON
+#define ILI9486_NORON  0x13  ///< Normal Display Mode ON
+
+#define ILI9486_RDMODE     0x0A  ///< Read Display Power Mode
+#define ILI9486_RDMADCTL   0x0B  ///< Read Display MADCTL
+#define ILI9486_RDPIXFMT   0x0C  ///< Read Display Pixel Format
+#define ILI9486_RDIMGFMT   0x0D  ///< Read Display Image Format
+#define ILI9486_RDSELFDIAG 0x0F  ///< Read Display Self-Diagnostic Result
+
+#define ILI9486_INVOFF 0x20  ///< Display Inversion OFF
+#define ILI9486_INVON  0x21  ///< Display Inversion ON
+//#define ILI9486_GAMMASET 0x26 ///< Gamma Set
+#define ILI9486_DISPOFF 0x28  ///< Display OFF
+#define ILI9486_DISPON  0x29  ///< Display ON
+
+#define ILI9486_CASET 0x2A  ///< Column Address Set
+#define ILI9486_PASET 0x2B  ///< Page Address Set
+#define ILI9486_RAMWR 0x2C  ///< Memory Write
+#define ILI9486_RAMRD 0x2E  ///< Memory Read
+
+#define ILI9486_PTLAR    0x30  ///< Partial Area
+#define ILI9486_VSCRDEF  0x33  ///< Vertical Scrolling Definition
+#define ILI9486_MADCTL   0x36  ///< Memory Access Control
+#define ILI9486_VSCRSADD 0x37  ///< Vertical Scrolling Start Address
+#define ILI9486_PIXFMT   0x3A  ///< COLMOD: Pixel Format Set
+
+#define ILI9486_FRMCTR1 0xB1  ///< Frame Rate Control (In Normal Mode/Full Colors)
+#define ILI9486_FRMCTR2 0xB2  ///< Frame Rate Control (In Idle Mode/8 colors)
+#define ILI9486_FRMCTR3 0xB3  ///< Frame Rate control (In Partial Mode/Full Colors)
+#define ILI9486_INVCTR  0xB4  ///< Display Inversion Control
+#define ILI9486_DFUNCTR 0xB6  ///< Display Function Control
+
+#define ILI9486_PWCTR1 0xC0  ///< Power Control 1
+#define ILI9486_PWCTR2 0xC1  ///< Power Control 2
+#define ILI9486_PWCTR3 0xC2  ///< Power Control 3
+#define ILI9486_PWCTR4 0xC3  ///< Power Control 4
+#define ILI9486_PWCTR5 0xC4  ///< Power Control 5
+#define ILI9486_VMCTR1 0xC5  ///< VCOM Control 1
+#define ILI9486_VMCTR2 0xC7  ///< VCOM Control 2
+
+#define ILI9486_RDID1 0xDA  ///< Read ID 1
+#define ILI9486_RDID2 0xDB  ///< Read ID 2
+#define ILI9486_RDID3 0xDC  ///< Read ID 3
+#define ILI9486_RDID4 0xD3  ///< Read ID 4
+
+#define ILI9486_GMCTRP1 0xE0  ///< Positive Gamma Correction
+#define ILI9486_GMCTRN1 0xE1  ///< Negative Gamma Correction
+//#define ILI9486_PWCTR6     0xFC
+
+#define ILI9486_DGAMMA1 0xE2  ///< Digital Gamma Control 1
+#define ILI9486_DGAMMA2 0xE3  ///< Digital Gamma Control 2
+
+// Color definitions
+#define ILI9486_BLACK       0x0000  ///<   0,   0,   0
+#define ILI9486_NAVY        0x000F  ///<   0,   0, 123
+#define ILI9486_DARKGREEN   0x03E0  ///<   0, 125,   0
+#define ILI9486_DARKCYAN    0x03EF  ///<   0, 125, 123
+#define ILI9486_MAROON      0x7800  ///< 123,   0,   0
+#define ILI9486_PURPLE      0x780F  ///< 123,   0, 123
+#define ILI9486_OLIVE       0x7BE0  ///< 123, 125,   0
+#define ILI9486_LIGHTGREY   0xC618  ///< 198, 195, 198
+#define ILI9486_DARKGREY    0x7BEF  ///< 123, 125, 123
+#define ILI9486_BLUE        0x001F  ///<   0,   0, 255
+#define ILI9486_GREEN       0x07E0  ///<   0, 255,   0
+#define ILI9486_CYAN        0x07FF  ///<   0, 255, 255
+#define ILI9486_RED         0xF800  ///< 255,   0,   0
+#define ILI9486_MAGENTA     0xF81F  ///< 255,   0, 255
+#define ILI9486_YELLOW      0xFFE0  ///< 255, 255,   0
+#define ILI9486_WHITE       0xFFFF  ///< 255, 255, 255
+#define ILI9486_ORANGE      0xFD20  ///< 255, 165,   0
+#define ILI9486_GREENYELLOW 0xAFE5  ///< 173, 255,  41
+#define ILI9486_PINK        0xFC18  ///< 255, 130, 198
+
 class ILI9486: public Adafruit_GFX {
  public:
-  ILI9486();
-  ~ILI9486(void);
+  ILI9486(uint8_t d0, uint8_t rd, uint8_t wr, uint8_t dc, int8_t cs = -1, int8_t rst = -1);
 
-  bool begin(uint8_t switchvcc = SSD1306_SWITCHCAPVCC, uint8_t i2caddr = 0, bool reset = true, bool periphBegin = true);
-  void display(void);
-  void clearDisplay(void);
-  void invertDisplay(bool i);
-  void dim(bool dim);
+  ILI9486(uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, uint8_t rd,
+          uint8_t wr, uint8_t dc, int8_t cs = -1, int8_t rst = -1);
+
+  bool begin(bool reset = true);
   void drawPixel(int16_t x, int16_t y, uint16_t color);
-  virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
-  virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
-  void         startscrollright(uint8_t start, uint8_t stop);
-  void         startscrollleft(uint8_t start, uint8_t stop);
-  void         startscrolldiagright(uint8_t start, uint8_t stop);
-  void         startscrolldiagleft(uint8_t start, uint8_t stop);
-  void         stopscroll(void);
-  void         ssd1306_command(uint8_t c);
-  bool         getPixel(int16_t x, int16_t y);
-  uint8_t*     getBuffer(void);
 
  private:
-}
+  void    setBounds(uint16_t xStart, uint16_t yStart, uint16_t xEnd, uint16_t yEnd);
+  void    sendCommand(uint8_t command);
+  void    sendData(uint8_t data);
+  uint8_t readData();
+  void    startTransaction();
+  void    endTransaction();
+  void    sendFrame(uint8_t command, uint8_t length, uint8_t data[]);
+
+  bool    _useChipSelect, _useReset;
+  uint8_t _dataPins[8], _readPin, _writePin, _dataCommandPin, _chipSelectPin, _resetPin;
+};
 
 #endif  // __ILI9486_H__
